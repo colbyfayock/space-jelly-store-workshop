5f3fc22ea2376b0315f09450c9087ef7dad3b112
Colby Fayock <fayock@gmail.com>
2020-11-03 10:31:18 -0500

    solution 02 and adding 03

---
+++ lessons/03 - Add and Configure Products in the Stripe Dashboard for an Online Store/README.md
@@ -0,0 +1,24 @@
+# Lesson 02: Add a Grid of Products with Images to a Next.js React App
+
+The core of any shopping experience is simply being able to see what the store has to offer! This includes what products are available and how much each one of them costs.
+
+While we can do this in a variety of ways, a common way is to show a grid of products where we can see everything the store has to offer (or a lot of it). Using some HTML and CSS, we can create our product grid allowing our customers a look into our store.
+
+## Getting Started
+
+For our second lesson, we're going to put together a product grid using some HTML and CSS.
+
+We'll start off by updating the default Next.js template to include our products instead of links to the Next.js docs. Then, we'll import some images for our products that will already be available in this lesson folder so we can show what our products look like. Finally, we'll make some basic CSS changes to the default Next.js template to make sure we're providing a good experience to our customers.
+
+## Objectives
+* Add products to the app
+* Import product images and add to each product
+* Update the default page styles
+
+## Exercises
+
+### 1. Add some products to the homepage of an online store
+
+### 2. Import local images into the app for each product
+
+### 3. Update the app styles for a better user experience
---
+++ lessons/03 - Add and Configure Products in the Stripe Dashboard for an Online Store/pages/solution.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import styles from '../styles/Home.solution.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Space Jelly Shop
+        </h1>
+
+        <p className={styles.description}>
+          The best space jellyfish swag on the web!
+        </p>
+
+        <ul className={styles.grid}>
+          <li className={styles.card}>
+            <a href="#">
+              <img src="/images/spacejelly-tshirt.jpg" alt="Space Jelly Tshirt" />
+              <h3>Space Jelly Tshirt</h3>
+              <p>Bring Cosmo the space Jellyfish to your wardrobe with this high quality tshirt.</p>
+            </a>
+          </li>
+
+          <li className={styles.card}>
+            <a href="#">
+              <img src="/images/spacejelly-stickers.jpg" alt="Space Jelly Stickers" />
+              <h3>Space Jelly Stickers</h3>
+              <p>Add some flare to your laptop with a sticker of Cosmo the Space Jellyfish.</p>
+            </a>
+          </li>
+
+          <li className={styles.card}>
+            <a href="#">
+              <img src="/images/spacejelly-combo.jpg" alt="Space Jelly Combo Pack" />
+              <h3>Space Jelly Combo</h3>
+              <p>Show your love for Cosmo with a tshirt and sticker combo pack!</p>
+            </a>
+          </li>
+        </ul>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
